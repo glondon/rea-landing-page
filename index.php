c5438933ea8abf3967042fa043d34768bdb83b1a
Greg London <gregdlondon@gmail.com>
2016-09-09 00:34:59 -0500

    Began adding meta tags to <head>

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">   
+    <meta name="description" content="Buy a new houst with Real Estate Agent in Virginia Beach.">
+    <meta name="author" content="Greg London">
+	<title>Buy a house with Real Estate Agent David Boord in Virginia Beach, VA.</title>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+	<style>
+		/*custom styles here*/
+	</style>
 
 </head>
 
