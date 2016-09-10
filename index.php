6e557593fc41b2b35fce386ac57a197b73fecb6b
Greg London <gregdlondon@gmail.com>
2016-09-09 21:12:09 -0500

    Fixed meta spacing.

--- index.php
+++ index.php
@@ -11,15 +11,14 @@ include 'config.php';
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1">   
-    <meta name="description" content="<?php echo $description ?>">
-    <meta name="author" content="Greg London">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php echo $description ?>">
+	<meta name="author" content="Greg London">
 	<title><?php echo $title ?></title>
-
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 	<style>
-		/*custom styles here*/
+		
 	</style>
 
 </head>
@@ -27,6 +26,16 @@ include 'config.php';
 <body>
 
 	<h1>Buy it now!</h1>
+
+	<form>
+		<fieldset>
+
+		</fieldset>
+	</form>
+
+	<script type="text/javascript">
+
+	</script>
 
 </body>
 
