3b46ddb40b3e472d3e8cd25514883fd6ce5690f6
Greg London <gregdlondon@gmail.com>
2016-09-10 11:33:21 -0500

    Began validating form.

--- submit.php
+++ submit.php
@@ -3,4 +3,42 @@
 * Author: Greg London
 * http://greglondon.info
 */
+
+include 'config.php';
+
+if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest')
+	exit(header('Location: '.URL.''));
+
+session_start();
+
+if(!empty($_POST)){
+
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$phone = trim($_POST['phone']);
+
+	$errors = array();
+
+	if($name == '' || $name == 'Name')
+		$errors['name'] = 'The name field is required!';
+	else
+		$errors['name'] = null;
+
+	if($email == '' || $email == 'Email')
+		$errors['email'] = 'The email field is required!';
+	elseif(filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+		$errors['email'] = $email . ' is not a valid email address!';
+	else
+		$errors['email'] = null;
+
+	if($phone == '' || $phone == 'Phone')
+		$errors['phone'] = 'The phone field is required!';
+	elseif(!preg_match('/^\(?[0-9]{3}\)?[-. ]?[0-9]{3}[-. ]?[0-9]{4}$/', $phone))
+		$errors['phone'] = $phone . ' is not a correctly formatted phone value!';
+	else
+		$errors['phone'] = null;
+
+	
+}
+
 ?>
